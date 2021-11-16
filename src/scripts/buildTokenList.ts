0dda57732a935eadab83d176c5c30df2a8db0d69
Larry Jarry <larry@quarry.so>
2021-11-17 06:48:51 +0800

    update token list

--- src/scripts/buildTokenList.ts
+++ src/scripts/buildTokenList.ts
@@ -21,6 +21,8 @@ const dedupeTokenList = (tokens: TokenInfo[]): TokenInfo[] => {
 };
 
 const TOKEN_LIST_URLS = [
+  "https://raw.githubusercontent.com/saber-hq/saber-lp-token-list/master/lists/saber-lp.mainnet-beta.json",
+  "https://raw.githubusercontent.com/saber-hq/saber-lp-token-list/master/lists/saber-lp.devnet.json",
   "https://registry.saber.so/data/token-list.mainnet.json",
   "https://registry.saber.so/data/token-list.devnet.json",
   "https://raw.githubusercontent.com/solana-labs/token-list/main/src/tokens/solana.tokenlist.json",
