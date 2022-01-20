2d42c4615034d606fcbdce02100f67d93e48ca1f
michaelhly <michaelhly@gmail.com>
2022-01-20 02:13:14 -0800

    Add saros lp token list to TOKEN_LIST_URLS

--- src/constants.ts
+++ src/constants.ts
@@ -5,6 +5,7 @@ export const TOKEN_LIST_URLS = [
   "https://raw.githubusercontent.com/saber-hq/saber-lp-token-list/master/lists/saber-lp.devnet.json",
   "https://registry.saber.so/data/token-list.mainnet.json",
   "https://registry.saber.so/data/token-list.devnet.json",
+  "https://raw.githubusercontent.com/coin98/saros-lp-token-list/master/lists/saros-lp.mainnet.json",
   "https://raw.githubusercontent.com/solana-labs/token-list/main/src/tokens/solana.tokenlist.json",
 ];
 
