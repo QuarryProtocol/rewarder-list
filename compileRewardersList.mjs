77a192bce43a67875303305766252dd9f0ed0571
Ian Macalinao <ian@saber.so>
2023-12-02 00:24:10 +0700

    mkdir

--- compileRewardersList.mjs
+++ compileRewardersList.mjs
@@ -10,6 +10,7 @@ const __dirname = path.dirname(__filename);
 const main = async () => {
   const raw = await fs.readFile(`${__dirname}/Rewarders.toml`);
   const data = toml.parse(raw.toString());
+  await fs.mkdir(`${__dirname}/src/config`, { recursive: true });
   await fs.writeFile(
     `${__dirname}/src/config/rewarder-list.json`,
     JSON.stringify(data, null, 2),
